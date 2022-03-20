5559ae6aadd79d6851dffe72e3d857c0c1444471
SIJUN YAN <90133908+SijunYan@users.noreply.github.com>
2022-03-20 15:23:50 +1100

    init react native app

---
+++ screens/Dashboard/MainLayout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Animated,
+} from "react-native-web";
+
+import { Home, Profile, Search } from "../../screens";
+import { COLORS, SIZES, FONTS, constants } from "../../constants";
+
+const MainLayout = () => {
+  return <View>MainLayout</View>;
+};
+
+export default MainLayout;
